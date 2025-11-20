e08ae1522a86aa4fbcaeb1e2bfede924c2da5c1b
mkumar-01 <manoj.kumar@nagarro.com>
2025-11-20 18:41:28 +0530

    fix bootstraping context problem

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,14 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(App, config);
+// const bootstrap = () => bootstrapApplication(App, config);
+
+// export default bootstrap;
+// export default function bootstrap() {
+//     return bootstrapApplication(App, config);
+// }
+
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(App, config, context);
 
 export default bootstrap;
